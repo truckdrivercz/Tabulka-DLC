c428018952701cf13613791dc03e67ca79d1aacf
truckdrivercz <eda.orvos@seznam.cz>
2025-09-19 16:21:38 +0200

    update

--- res/js/main.js
+++ res/js/main.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("./res/data/data.json")
+    let json = "./res/data/data.json";
+    fetch(json)
         .then(response => response.json())
         .then(data => {
             const tabulka = document.getElementById("tabulka");
